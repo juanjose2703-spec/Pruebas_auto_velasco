528a6b560318f71061346a68aee92a2f90e067cb
Juanfepm <juanfelipe210176@gmail.com>
2026-05-19 10:43:10 -0500

    Añadir funcion autores-paises

--- services/autores/app.py
+++ services/autores/app.py
@@ -2,6 +2,7 @@ from conexion import *
 from autores import mis_autores
 from services.paises import mis_paises
 
+
 programa = Flask(__name__)
 api = Api(programa)
 
@@ -17,11 +18,12 @@ class ListaAutores(Resource):
             resultado = mis_paises.consultar(nuevo["idPais"])
             if len(resultado)>0:
                 mis_autores.agregar(nuevo["idAutor"], nuevo["nombre"], nuevo ["email"], nuevo["idPais"])
-                return jsonify ({"mensaje": "Autor agregado con éxito"})
+                return jsonify({"mensaje": "Autor agregado con éxito"})
             else:
                 return jsonify({"mensaje": "Id de pais no existente"})
         else:
             return jsonify({"mensaje": "Id de autor ya existe"})
+
         
 class Autor(Resource):
     def get(self, idAutor):
